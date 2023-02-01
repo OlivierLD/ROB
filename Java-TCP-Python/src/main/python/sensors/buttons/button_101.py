59d77e1749c6d6cd962290f3f5c8b36b6e187928
OlivierLD <olivier@lediouris.net>
2023-02-01 15:44:41 +0100

    Doc, code, polishing.

--- Java-TCP-Python/src/main/python/sensors/buttons/button_101.py
+++ Java-TCP-Python/src/main/python/sensors/buttons/button_101.py
@@ -1,19 +1,33 @@
-
+#
 import time
 import board
 from digitalio import DigitalInOut, Direction, Pull
 
-# D20 is pin #
-btn = DigitalInOut(board.D20)  # DigitalInOut(board.SWITCH)
+# D20 is pin #38
+btn: DigitalInOut = DigitalInOut(board.D20)  # DigitalInOut(board.SWITCH)
+# print(f"Button is a {type(btn)}")
 btn.direction = Direction.INPUT
 btn.pull = Pull.UP
 
-while True:
-    if not btn.value:
-        print("BTN is down")
-    else:
-        #print("BTN is up")
-        pass
+print("Press button connected on GPIO-20")
 
-    time.sleep(0.1) # sleep for debounce
+prev_state: bool = btn.value
+# print(f"Button State is a {type(prev_state)}")
 
+keep_looping: bool = True
+while keep_looping:
+    try:
+        cur_state: bool = btn.value
+        if cur_state != prev_state:
+            if not cur_state:
+                print("BTN is down")  # Broadcast wherever needed
+            else:
+                print("BTN is up")    # Broadcast wherever needed
+        prev_state = cur_state
+
+        time.sleep(0.1) # sleep for debounce
+    except KeyboardInterrupt as oops:
+        print("\nUser interrupted")
+        keep_looping = False
+
+print("Bye!")
