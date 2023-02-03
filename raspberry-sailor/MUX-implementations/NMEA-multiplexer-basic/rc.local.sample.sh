2ebb34eccce1e50fa3087a2e17f8315ef67b87ea
OlivierLD <olivier@lediouris.net>
2023-02-03 16:42:42 +0100

    Scripts.

--- raspberry-sailor/MUX-implementations/NMEA-multiplexer-basic/rc.local.sample.sh
+++ raspberry-sailor/MUX-implementations/NMEA-multiplexer-basic/rc.local.sample.sh
@@ -6,9 +6,11 @@
 # sudo ln -s /dev/ttyACM0 /dev/ttyS80
 #
 # Start the REST Server for BME280
-~pi/repos/ROB/raspberry-sailor/NMEA-multiplexer/scripts/start.BME280.REST.server.sh --interactive:false  --machine-name:$(hostname -I) --port:9876 --verbose:false &
+~pi/repos/ROB/raspberry-sailor/NMEA-multiplexer/scripts/start.BME280.REST.server.sh --interactive:false  --machine-name:$(hostname -I) --port:9876 --verbose:false > bme280.python.log 2>&1 &
+sleep 5
 # Start the REST Server for SSD1306 (v2)
-~pi/repos/ROB/raspberry-sailor/NMEA-multiplexer/scripts/start.SSD1306.REST.server.v2.sh --interactive:false  --machine-name:$(hostname -I) --port:8080 --verbose:false --height:64 --wiring:SPI --data:NAV,POS,SOG,COG,ATM &
+~pi/repos/ROB/raspberry-sailor/NMEA-multiplexer/scripts/start.SSD1306.REST.server.v2.sh --interactive:false  --machine-name:$(hostname -I) --port:8080 --verbose:false --height:64 --wiring:SPI --data:NAV,POS,SOG,COG,ATM > ssd1306.python.log 2>&1 &
+sleep 5
 # Start the MUX
 pushd ~pi/repos/ROB/raspberry-sailor/MUX-implementations/NMEA-multiplexer-basic
 ./mux.sh nmea.mux.gps.sensor.nmea-fwd.yaml &
