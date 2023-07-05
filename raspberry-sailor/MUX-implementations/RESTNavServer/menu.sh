7642cc984b8668a8af7f5af867a0a278cf7862f2
OlivierLD <olivier@lediouris.net>
2023-07-05 16:46:44 +0200

    Reworked menus and so.

--- raspberry-sailor/MUX-implementations/RESTNavServer/menu.sh
+++ raspberry-sailor/MUX-implementations/RESTNavServer/menu.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #
-echo -e "+----------- Top level menu  ------------+"
-echo -e "| 1 - Multiplexer Demos                  |"
-echo -e "+----------------------------------------+"
-echo -e "| 2 - T900 (Dieumegard & Bataille)       | "
-echo -e "| 3 - Celestial Almanacs publishing      |"
-echo -e "| 4 - Long Term Almanacs publishing      |"
-echo -e "| 5 - Tables de Corrections des Hauteurs |"
-echo -e "+----------------------------------------+"
-echo -e "| Q - Quit                               |"
-echo -e "+----------------------------------------+"
+echo -e "+----------- T O P   L E V E L   M E N U ------------+"
+echo -e "| 1 - Multiplexer Demos                              |"
+echo -e "+----------------------------------------------------+"
+echo -e "| 2 - T900 (Dieumegard & Bataille)                   |"
+echo -e "| 3 - Celestial Almanacs publishing                  |"
+echo -e "|   - Can also be published from some Web Interfaces |"
+echo -e "| 4 - Long Term Almanacs publishing                  |"
+echo -e "| 5 - Tables de Corrections des Hauteurs             |"
+echo -e "+----------------------------------------------------+"
+echo -e "| Q - Quit                                           |"
+echo -e "+----------------------------------------------------+"
 echo -en "You choose > "
 read RESP
 case "${RESP}" in
