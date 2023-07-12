c1bb4afe49c64dc5d103081d2345d9219901459f
OlivierLD <olivier@lediouris.net>
2023-07-12 14:58:33 +0200

    Small config changes.

---
+++ raspberry-sailor/MUX-implementations/NMEA-multiplexer-basic/start.all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+stty -F /dev/ttyACM0 raw 4800 cs8 clocal
+#
+# Start MUX and Co on startup
+#
+# Start the REST Server for BME280
+/home/pi/nmea-dist/python/scripts/start.BME280.REST.server.sh --interactive:false  --machine-name:localhost --port:9876 --verbose:false > /home/pi/nmea-dist/bme280.python.log 2>&1
+sleep 10
+# Start the REST Server for SSD1306 (v2)
+/home/pi/nmea-dist/python/scripts/start.SSD1306.REST.server.v2.sh --interactive:false  --machine-name:localhost --port:8080 --verbose:false --height:64 --wiring:SPI --data:NAV,POS,SOG,COG,ATM --screen-saver:on > /home/pi/nmea-dist/ssd1306.python.log 2>&1
+sleep 10
+# Start the MUX
+cd /home/pi/nmea-dist
+nohup ./mux.sh nmea.mux.gps.sensor.2.nmea-fwd.yaml &
+
