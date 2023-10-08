6fd6c71e122c2e587897414a0ebfabf3a3946bda
OlivierLD <olivier@lediouris.net>
2023-10-08 08:47:44 +0200

    Script

--- raspberry-sailor/MUX-implementations/NMEA-multiplexer-basic/show.processes.sh
+++ raspberry-sailor/MUX-implementations/NMEA-multiplexer-basic/show.processes.sh
@@ -2,9 +2,16 @@
 #
 # Show server processes (MUX, Python-REST, Python-TCP)
 #
+echo -e "+-----------------------+"
+echo -e "| NMEA Multiplexer(s) : |"
+echo -e "+-----------------------+"
 ps -ef | grep GenericNMEAMultiplexer | grep -v grep | grep -v killns
 #
-ps -ef | grep python | grep REST 
+echo -e "+-----------------------+"
+echo -e "| Python serve(s) :     |"
+echo -e "+-----------------------+"
+ps -ef | grep python | grep REST
 #
 ps -ef | grep python | grep TCP_ 
-
+#
+echo -e "-----------------------------"
