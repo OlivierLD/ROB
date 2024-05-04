c71a858958b75fbdb1891a50cdb84a2da285efac
OlivierLD <olivier@lediouris.net>
2024-05-04 16:27:40 +0200

    Python Unit Tests

--- astro-computer/AstroComputer/src/test/python/CelestialTest.py
+++ astro-computer/AstroComputer/src/test/python/CelestialTest.py
@@ -8,15 +8,19 @@
 import unittest
 
 import os, sys, time
-from datetime import datetime, timezone
+# from datetime import datetime, timezone
+
+VERBOSE: bool = False
 
 # First, set the path
 current_path = os.path.dirname(os.path.abspath(__file__))
-print(f"Absolute current path ${current_path}")
+if VERBOSE:
+    print(f"Absolute current path ${current_path}")
 sys.path.append(current_path)
 sys.path.append(current_path + "/../../main/python/src/celestial_almanac")
 
-print(f"SysPath: ${sys.path}")
+if VERBOSE:
+    print(f"SysPath: ${sys.path}")
 
 # Now we can import thew required modules.
 from long_term_almanac import LongTermAlmanac as lta
@@ -34,7 +38,6 @@ hours: int = 16
 minutes: int = 50
 seconds: int = 20
 
-
 class MyTestCase(unittest.TestCase):
     def test_celest(self):
         before: int = int(round(time.time() * 1000))
@@ -42,12 +45,14 @@ class MyTestCase(unittest.TestCase):
         lta.calculate(year, month, day, hours, minutes, seconds, deltaT)
         after: int = int(round(time.time() * 1000))
         # Display results
-        print("----------------------------------------------")
-        print(f"Calculations done for {year}-{month:02d}-{day:02d} {hours:02d}:{minutes:02d}:{seconds:02d} UTC")
-        print("In {} ms".format(after - before))
-        print("----------------------------------------------")
+        if VERBOSE:
+            print("----------------------------------------------")
+            print(f"Calculations done for {year}-{month:02d}-{day:02d} {hours:02d}:{minutes:02d}:{seconds:02d} UTC")
+            print("In {} ms".format(after - before))
+            print("----------------------------------------------")
 
-        print("Sideral Time: {}".format(lta.SidTm))
+        if VERBOSE:
+            print("Sideral Time: {}".format(lta.SidTm))
 
         # Sun
         fmtGHASun: str = lta.outHA(lta.GHASun)
@@ -56,7 +61,8 @@ class MyTestCase(unittest.TestCase):
         fmtSDSun: str = lta.outSdHp(lta.SDSun)
         fmtHPSun: str = lta.outSdHp(lta.HPSun)
 
-        print("Sun: GHA {}, RA {}, DEC {}, sd {}, hp {}".format(fmtGHASun, fmtRASun, fmtDECSun, fmtSDSun, fmtHPSun))
+        if VERBOSE:
+            print("Sun: GHA {}, RA {}, DEC {}, sd {}, hp {}".format(fmtGHASun, fmtRASun, fmtDECSun, fmtSDSun, fmtHPSun))
 
         # Venus
         fmtGHAVenus: str = lta.outHA(lta.GHAVenus)
@@ -65,7 +71,8 @@ class MyTestCase(unittest.TestCase):
         fmtSDVenus: str = lta.outSdHp(lta.SDVenus)
         fmtHPVenus: str = lta.outSdHp(lta.HPVenus)
 
-        print("Venus: GHA {}, RA {}, DEC {}, sd {}, hp {}".format(fmtGHAVenus, fmtRAVenus, fmtDECVenus, fmtSDVenus, fmtHPVenus))
+        if VERBOSE:
+            print("Venus: GHA {}, RA {}, DEC {}, sd {}, hp {}".format(fmtGHAVenus, fmtRAVenus, fmtDECVenus, fmtSDVenus, fmtHPVenus))
 
         # Mars
         fmtGHAMars: str = lta.outHA(lta.GHAMars)
@@ -74,7 +81,8 @@ class MyTestCase(unittest.TestCase):
         fmtSDMars: str = lta.outSdHp(lta.SDMars)
         fmtHPMars: str = lta.outSdHp(lta.HPMars)
 
-        print("Mars: GHA {}, RA {}, DEC {}, sd {}, hp {}".format(fmtGHAMars, fmtRAMars, fmtDECMars, fmtSDMars, fmtHPMars))
+        if VERBOSE:
+            print("Mars: GHA {}, RA {}, DEC {}, sd {}, hp {}".format(fmtGHAMars, fmtRAMars, fmtDECMars, fmtSDMars, fmtHPMars))
 
         # Jupiter
         fmtGHAJupiter: str = lta.outHA(lta.GHAJupiter)
@@ -83,7 +91,8 @@ class MyTestCase(unittest.TestCase):
         fmtSDJupiter: str = lta.outSdHp(lta.SDJupiter)
         fmtHPJupiter: str = lta.outSdHp(lta.HPJupiter)
 
-        print("Jupiter: GHA {}, RA {}, DEC {}, sd {}, hp {}".format(fmtGHAJupiter, fmtRAJupiter, fmtDECJupiter, fmtSDJupiter, fmtHPJupiter))
+        if VERBOSE:
+            print("Jupiter: GHA {}, RA {}, DEC {}, sd {}, hp {}".format(fmtGHAJupiter, fmtRAJupiter, fmtDECJupiter, fmtSDJupiter, fmtHPJupiter))
 
         # Saturn
         fmtGHASaturn: str = lta.outHA(lta.GHASaturn)
@@ -92,7 +101,8 @@ class MyTestCase(unittest.TestCase):
         fmtSDSaturn: str = lta.outSdHp(lta.SDSaturn)
         fmtHPSaturn: str = lta.outSdHp(lta.HPSaturn)
 
-        print("Saturn: GHA {}, RA {}, DEC {}, sd {}, hp {}".format(fmtGHASaturn, fmtRASaturn, fmtDECSaturn, fmtSDSaturn, fmtHPSaturn))
+        if VERBOSE:
+            print("Saturn: GHA {}, RA {}, DEC {}, sd {}, hp {}".format(fmtGHASaturn, fmtRASaturn, fmtDECSaturn, fmtSDSaturn, fmtHPSaturn))
 
         # Moon
         fmtGHAMoon: str = lta.outHA(lta.GHAMoon)
@@ -101,41 +111,49 @@ class MyTestCase(unittest.TestCase):
         fmtSDMoon: str = lta.outSdHp(lta.SDMoon)
         fmtHPMoon: str = lta.outSdHp(lta.HPMoon)
 
-        print("Moon: GHA {}, RA {}, DEC {}, sd {}, hp {}".format(fmtGHAMoon, fmtRAMoon, fmtDECMoon, fmtSDMoon, fmtHPMoon))
-        print("\tMoon phase {} -> {}".format(lta.moonPhaseAngle, lta.moonPhase))
+        if VERBOSE:
+            print("Moon: GHA {}, RA {}, DEC {}, sd {}, hp {}".format(fmtGHAMoon, fmtRAMoon, fmtDECMoon, fmtSDMoon, fmtHPMoon))
+            print("\tMoon phase {} -> {}".format(lta.moonPhaseAngle, lta.moonPhase))
 
         # Polaris
         fmtGHAPolaris: str = lta.outHA(lta.GHAPol)
         fmtRAPolaris: str = lta.outRA(lta.RAPol)
         fmtDECPolaris: str = lta.outDec(lta.DECPol)
 
-        print("Polaris: GHA {}, RA {}, DEC {}".format(fmtGHAPolaris, fmtRAPolaris, fmtDECPolaris))
+        if VERBOSE:
+            print("Polaris: GHA {}, RA {}, DEC {}".format(fmtGHAPolaris, fmtRAPolaris, fmtDECPolaris))
 
         # Obliquity of Ecliptic
         OoE: str = lta.outECL(lta.eps0)
         tOoE: str = lta.outECL(lta.eps)
 
-        print("Ecliptic: obliquity {}, true {}".format(OoE, tOoE))
+        if VERBOSE:
+            print("Ecliptic: obliquity {}, true {}".format(OoE, tOoE))
 
         # Equation of time
         fmtEoT: str = lta.outEoT(lta.EoT)
-        print("Equation of time {} ({} m)".format(fmtEoT, lta.EoT))
+        if VERBOSE:
+            print("Equation of time {} ({} m)".format(fmtEoT, lta.EoT))
         tPass: float = (12.0 * 60) - lta.EoT  # In minutes. TODO Check the EoT sign...
         tPass /= 60.0  # In hours
         tPassHours: int = int(tPass)
         tPassMinutes: int = int((tPass*60) % 60)
         tPassSeconds: float = (tPass*3600) % 60
-        print("TPass Sun: {}h {}m {:.2f}s".format(tPassHours, tPassMinutes, tPassSeconds))
+        if VERBOSE:
+            print("TPass Sun: {}h {}m {:.2f}s".format(tPassHours, tPassMinutes, tPassSeconds))
 
 
         # Lunar Distance of Sun
         fmtLDist: str = lta.outHA(lta.LDist)
-        print("Lunar Distance: {}".format(fmtLDist))
+        if VERBOSE:
+            print("Lunar Distance: {}".format(fmtLDist))
 
-        print("Day of Week: {}".format(lta.DoW))
-        print("Done with Python!")
+        if VERBOSE:
+            print("Day of Week: {}".format(lta.DoW))
+            print("Done with Python!")
 
         self.assertEqual(lta.DoW, "SAT")
+        self.assertEqual(lta.GHASun, 71.36350580844164)
 
 
 if __name__ == '__main__':
