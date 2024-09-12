83999724ef67a8619f0f8767ff1d28d50a1c2dbc
OlivierLD <olivier@lediouris.net>
2024-09-12 08:13:15 +0200

    UDP Clean up, and samples.

--- raspberry-sailor/MUX-implementations/RESTNavServer/launchers/demoLauncher.sh
+++ raspberry-sailor/MUX-implementations/RESTNavServer/launchers/demoLauncher.sh
@@ -99,6 +99,7 @@ URL_OPTION_13c="http://localhost:${HTTP_PORT}/web/chartless.gps.html"
 URL_OPTION_13d="http://localhost:${HTTP_PORT}/web/chartless.world.data.html"
 URL_OPTION_13e="http://localhost:${HTTP_PORT}/web/chartless.world.data.html"
 URL_OPTION_13f="http://localhost:${HTTP_PORT}/web/chartless.world.data.html"
+URL_OPTION_13g="http://localhost:${HTTP_PORT}/web/chartless.world.data.html"
 #
 function openBrowser() {
   if [[ $(uname -s) == *Linux* ]]; then
@@ -167,6 +168,7 @@ while [[ "${GO}" == "true" ]]; do
 	echo -e "|                                                                                         | ${RED}13d${NC}. GPS only, AIS, Chartless Map (Etel-Groix). (demanding...)                          |"
 	echo -e "|                                                                                         | ${RED}13e${NC}. GPS only, Chartless Map (La Trinité-Groix). GPS Only.                              |"
 	echo -e "|                                                                                         | ${RED}13f${NC}. GPS only, Chartless Map (Kernével-La Trinité). GPS Only, no AIS.                   |"
+	echo -e "|                                                                                         | ${RED}13g${NC}. GPS only, Chartless Map (La Trinité-Groix). GPS Only, AIS from OpenCPN/UDP.        |"
 	echo -e "+-----------------------------------------------------------------------------------------+-----------------------------------------------------------------------------------------+"
 	echo -e "| ${RED}20${NC}.  Get Data Cache (curl)                                                              | ${RED}20b${NC}. Get REST operations list (curl)                                                    |"
 	echo -e "+-----------------------------------------------------------------------------------------+-----------------------------------------------------------------------------------------+"
@@ -329,6 +331,11 @@ while [[ "${GO}" == "true" ]]; do
 	      "13f")
 	        PROP_FILE=mux-configs/nmea.mux.replay.kernevel.la.trinite.yaml
 	      	displayHelp ${HELP_ON} ${PROP_FILE} ${URL_OPTION_13f}
+	        ;;
+	      "13g")
+	        PROP_FILE=mux-configs/nmea.mux.udp.ais.yaml
+	        echo -e "Use OpenCPN to forward AIS data through UDP:8002"
+	      	displayHelp ${HELP_ON} ${PROP_FILE} ${URL_OPTION_13g}
 	        ;;
 	      "20")
 	      	echo -e "Uses a 'curl' to display the current data cache, using REST"
@@ -929,6 +936,29 @@ while [[ "${GO}" == "true" ]]; do
 	    ;;
 	  "13f")
   	  PROP_FILE=mux-configs/nmea.mux.replay.kernevel.la.trinite.yaml
+	    echo -e "Launching Nav Server with ${PROP_FILE}"
+      # Ask to launch a browser in interactive mode (and not provided already)
+      # echo -e ">> Options: INTERACTIVE=[${INTERACTIVE}], LAUNCH_BROWSER=[${LAUNCH_BROWSER}], LNCH_BRWSR_PROVIDED=[${LNCH_BRWSR_PROVIDED}]"
+      if [[ "${INTERACTIVE}" == "Y" ]] && [[ "${LAUNCH_BROWSER}" == "N" ]] && [[ "${LNCH_BRWSR_PROVIDED}" == "N" ]]; then
+        echo -en "Launch a browser ? y|[n] > "
+        read REPLY
+        if [[ ${REPLY} =~ ^(yes|y|Y)$ ]]; then
+        LAUNCH_BROWSER=Y
+        echo -e ">> Will launch a browser"
+        fi
+      fi
+	    ./runNavServer.sh --mux:${PROP_FILE} --no-date ${NAV_SERVER_EXTRA_OPTIONS} &
+	    if [[ "${LAUNCH_BROWSER}" == "Y" ]] || [[ "${LAUNCH_BROWSER}" == "y" ]]; then
+		    echo -e ">>> Waiting for the server to start..."
+		    sleep 5 # Wait for the server to be operational
+		    openBrowser ${URL_OPTION_13e}
+		  else
+	    	echo -e "${RED}In a browser: http://localhost:${HTTP_PORT}/web/index.html${NC}"
+	    fi
+	    GO=false
+	    ;;
+	  "13g")
+  	  PROP_FILE=mux-configs/nmea.mux.udp.ais.yaml
 	    echo -e "Launching Nav Server with ${PROP_FILE}"
       # Ask to launch a browser in interactive mode (and not provided already)
       # echo -e ">> Options: INTERACTIVE=[${INTERACTIVE}], LAUNCH_BROWSER=[${LAUNCH_BROWSER}], LNCH_BRWSR_PROVIDED=[${LNCH_BRWSR_PROVIDED}]"
