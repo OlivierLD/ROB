f7988f66fce8898f7cc646b919b983cc922a14ea
OlivierLD <olivier@lediouris.net>
2025-07-02 08:09:15 +0200

    REST System updates.

--- raspberry-sailor/MUX-implementations/RESTSystemServer/start.server.sh
+++ raspberry-sailor/MUX-implementations/RESTSystemServer/start.server.sh
@@ -3,7 +3,7 @@
 # CP=../build/libs/RESTNavServer-1.0-all.jar
 CP=$(find . -name '*-all.jar')
 SUDO="sudo "
-JAVA_OPT="-Drest.verbose=true -Dhttp.port=1024"
+JAVA_OPTS="-Drest.verbose=true -Dhttp.port=1234"
 
 COMMAND="${SUDO}java -cp ${CP} ${JAVA_OPTS} systemrest.SystemServer"
 if [[ "${CMD_VERBOSE}" == "Y" || 1 -eq 1 ]]; then    # Always true...
